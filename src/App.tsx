d2a0724562d30f4a45b16196f92a8a5b0a6126dc
Jordan Hilado <jordanalihilado@gmail.com>
2022-01-12 22:42:01 -0800

    Upload template

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Main from "./screens/Blog";
+
+function App() {
+  return <Main />;
+}
+
+export default App;
