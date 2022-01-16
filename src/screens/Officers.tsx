0debb5068a118d8ccdfaa469515249b90027e1ff
Jordan Hilado <jordanalihilado@gmail.com>
2022-01-16 01:32:14 -0800

    added officer cards and json support

--- src/screens/Officers.tsx
+++ src/screens/Officers.tsx
@@ -4,6 +4,13 @@ import CssBaseline from "@mui/material/CssBaseline";
 import { Container } from "@mui/material";
 import Header from "./Header";
 import { sections } from "./Sections";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import officerInfo from "../assets/officerInfo.json";
 
 const theme = createTheme({
   typography: {
@@ -24,6 +31,45 @@ export default function Officers() {
         <Header title="CSULB AESB" sections={sections} />
         <main>
           <h1>Officers</h1>
+          <div>
+            {officerInfo.map((officer) => {
+              return (
+                <>
+                  <Card sx={{ maxWidth: 345 }}>
+                    <CardMedia
+                      component="img"
+                      alt="green iguana"
+                      height="140"
+                      image={officer.image}
+                    />
+                    <CardContent>
+                      <Typography gutterBottom variant="h5" component="div">
+                        {officer.role} {officer.name}
+                        <Typography
+                          gutterBottom
+                          variant="body2"
+                          component="div"
+                        >
+                          {officer.major}
+                        </Typography>
+                      </Typography>
+                      <Typography variant="body1" color="text.secondary">
+                        {officer.description}
+                      </Typography>
+                    </CardContent>
+                    <CardActions>
+                      <Button href={officer.email} size="small">
+                        Email
+                      </Button>
+                      <Button href={officer.linkedin} size="small">
+                        LinkedIn
+                      </Button>
+                    </CardActions>
+                  </Card>
+                </>
+              );
+            })}
+          </div>
         </main>
       </Container>
     </ThemeProvider>
