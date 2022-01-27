8e65f5e13d042b3a21f088432d5e4a9658e7c038
Jordan Hilado <jordanalihilado@gmail.com>
2022-01-26 23:47:48 -0800

    branch title

--- src/assets/color.tsx
+++ src/assets/color.tsx
@@ -1,6 +1,8 @@
 export const color = {
   primary: "#1d2e7d",
+  primary2: "#f542e9",
   secondary: "#fbdf7a",
+  secondary2: "#3fc479",
   dark: "#18191A",
   light: "#E4E6EB",
 };
--- src/screens/theme.tsx
+++ src/screens/theme.tsx
@@ -38,3 +38,79 @@ export const theme = createTheme({
     },
   },
 });
+
+export const light = createTheme({
+  typography: {
+    fontFamily: "Questrial",
+  },
+  palette: {
+    background: {
+      default: color.dark,
+    },
+  },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        text: {
+          color: color.primary2,
+          "&:hover": {
+            backgroundColor: color.primary2 + "20",
+          },
+        },
+        contained: {
+          backgroundColor: color.primary2,
+          "&:hover": {
+            backgroundColor: color.primary2 + "90",
+          },
+        },
+        outlined: {
+          color: color.primary2,
+          backgroundColor: color.primary2 + "10",
+          border: "1px solid " + color.primary2,
+          "&:hover": {
+            backgroundColor: color.primary2 + "20",
+            border: "1px solid " + color.primary2,
+          },
+        },
+      },
+    },
+  },
+});
+
+export const dark = createTheme({
+  typography: {
+    fontFamily: "Questrial",
+  },
+  palette: {
+    background: {
+      default: color.dark,
+    },
+  },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        text: {
+          color: color.primary,
+          "&:hover": {
+            backgroundColor: color.primary + "20",
+          },
+        },
+        contained: {
+          backgroundColor: color.primary,
+          "&:hover": {
+            backgroundColor: color.primary + "90",
+          },
+        },
+        outlined: {
+          color: color.primary,
+          backgroundColor: color.primary + "10",
+          border: "1px solid " + color.primary,
+          "&:hover": {
+            backgroundColor: color.primary + "20",
+            border: "1px solid " + color.primary,
+          },
+        },
+      },
+    },
+  },
+});
