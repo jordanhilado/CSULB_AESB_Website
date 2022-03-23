2f97153b19261a2c10c8d5c35923abd1b133c529
Jordan Hilado <jordanalihilado@gmail.com>
2022-03-23 13:34:08 -0700

    add blogpost

--- src/assets/blogInfo.tsx
+++ src/assets/blogInfo.tsx
@@ -1,5 +1,12 @@
 export const blogInfo = [
   {
+    title: "BeachHacks 6.0 Applications Now Open",
+    subtitle: "March 23, 2022 by Jordan H.",
+    body: "Association for Computing Machinery and the BeachHacks committees are proud to present BeachHacks 6.0: Success at the Beach! BeachHacks 6.0 will take place from April 9th – April 10th 2022. Apply at the link below!",
+    link: "https://beachhacks.com/",
+    linkname: "BeachHacks 6.0 Application"
+  },
+  {
     title: "Funding for COE Student Organizations",
     subtitle: "February 11, 2022 by Margo McCall",
     body: "CSULB College of Engineering student organizations may submit proposals to request supporting funds from the College of Engineering. The funding can support projects, events, conferences, or other club programs. Applications will be received until the funding is depleted. The review and approval process will take an average of 2-3 weeks – please plan accordingly. To submit a proposal, visit the link below.",
@@ -17,8 +24,10 @@ export const blogInfo = [
     body: "Welcome to Spring 2022! AESB now has a new revamped website. With the new semester in full swing, we have lots of events and meetings planned out for the semester that we will announce soon! Keep your eyes peeled!",
   },
   // {
-  //   title: "Blog Post 2",
-  //   subtitle: "February 1, 2022 by Jordan H.",
-  //   body: "Ut sit amet leo mauris. Sed aliquam tincidunt dui, eu mollis sem semper non. Nulla a tortor imperdiet, viverra est ac, blandit leo. Donec diam purus, convallis id fermentum ac, mattis id sapien. Nullam laoreet ante et est semper, ac egestas est tincidunt. Donec non commodo mauris, id tempor est. Nam dui purus, mollis id viverra in, viverra id ipsum. Aenean id venenatis dui. Vestibulum eget elit elementum, semper nibh placerat, aliquam sapien.",
+  //   title: "",
+  //   subtitle: "",
+  //   body: "",
+  //   link: "",
+  //   linkname: ""
   // },
 ];
